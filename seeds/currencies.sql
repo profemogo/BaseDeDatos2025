4b437e01a5719f89f760929af11dd99fc7973d50
Ender Puentes <endpuent@gmail.com>
2025-05-29 23:06:13 -0300

    feat: Add currency seeds for MVP to populate initial currency data in the database

---
+++ seeds/currencies.sql
@@ -0,0 +1,34 @@
+/**
+ * Expenses App
+ *
+ * Author: Ender Jose Puentes Vargas
+ * CI: V-25153102
+ *
+ * Currencies Seeds
+ * 
+ **/ 
+
+ -- Currencies for MVP
+
+INSERT INTO Currency (name, symbol) VALUES
+('Dólar Estadounidense', '$'),
+('Euro', '€'),
+('Libra Esterlina', '£'),
+('Dólar Canadiense', '$'),
+('Peso Mexicano', '$'),
+('Peso Colombiano', '$'),
+('Peso Chileno', '$'),
+('Peso Argentino', '$'),
+('Peso Uruguayo', '$'),
+('Guaraní Paraguayo', '₲'),
+('Real Brasileño', 'R$'),
+('Bolívar Digital Venezolano', 'Bs.'),
+('Sol Peruano', 'S/'),
+('Peso Boliviano', 'Bs'),
+('Peso Dominicano', 'RD$'),
+('Colón Costarricense', '₡'),
+('Quetzal Guatemalteco', 'Q'),
+('Lempira Hondureño', 'L'),
+('Córdoba Nicaragüense', 'C$'),
+('Balboa Panameño', 'B/.'),
+('Yuan Chino', '¥');
