61f9bb61f9189ac98ab21c5619a71aecbdc501ad
joselamas <morochojm03@gmail.com>
2025-05-30 05:45:09 +0100

    V2
    V2

--- 2_crear_LLavesForaneas.sql
+++ 2_crear_LLavesForaneas.sql
@@ -53,79 +53,79 @@ ALTER TABLE registro_colibri ADD CONSTRAINT FK_registro_colibri_estatus FOREIGN 
 
 
 /*tabla captura_colibri*/
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_captura_colibri_localidad') != 0
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_captura_colibri_localidad') 
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_captura_colibri_localidad
 		
 ALTER TABLE captura_colibri ADD CONSTRAINT FK_captura_colibri_localidad FOREIGN KEY (localidad_asociada) REFERENCES centro_registro_localidad(id);
 
 
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_captura_colibri_estatus') != 0	
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_captura_colibri_estatus') 	
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_captura_colibri_estatus
 
 ALTER TABLE captura_colibri ADD CONSTRAINT FK_captura_colibri_estatus FOREIGN KEY (estatus) REFERENCES leyenda_estatus(estatus);
 	
 
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_captura_colibri_idespecie') != 0
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_captura_colibri_idespecie') 
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_captura_colibri_idespecie
 	
 ALTER TABLE captura_colibri ADD CONSTRAINT FK_captura_colibri_idespecie FOREIGN KEY (jornada_Anillo) REFERENCES sesion_captura(id);
 	 
 
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_registro_colibri_body_molt') != 0 
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_registro_colibri_body_molt') 
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_registro_colibri_body_molt
 
 Alter Table captura_colibri ADD CONSTRAINT FK_registro_colibri_body_molt FOREIGN KEY (body_molt) REFERENCES leyenda_body_molt(id);
 	
 
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_registro_colibri_ff_molt') != 0
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_registro_colibri_ff_molt') 
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_registro_colibri_ff_molt
 		
 ALTER TABLE captura_colibri ADD CONSTRAINT FK_registro_colibri_ff_molt FOREIGN KEY (ff_molt) REFERENCES leyenda_ff_molt(id);
 
 
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_registro_colibri_WRP') != 0	
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_registro_colibri_WRP') 	
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_registro_colibri_WRP
 
 ALTER TABLE captura_colibri ADD CONSTRAINT FK_registro_colibri_WRP FOREIGN KEY (WRP) REFERENCES leyenda_wrp(id);
 
 
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_registro_colibri_sexo') != 0
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_registro_colibri_sexo')
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_registro_colibri_sexo
 		
 ALTER TABLE captura_colibri ADD CONSTRAINT FK_registro_colibri_sexo FOREIGN KEY (sexo) REFERENCES leyenda_wrp(id);
 
 
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_registro_colibri_cor_pico') != 0
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_registro_colibri_cor_pico') 
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_registro_colibri_cor_pico
 		
 ALTER TABLE captura_colibri ADD CONSTRAINT FK_registro_colibri_cor_pico FOREIGN KEY (cor_pico) REFERENCES leyenda_cor_pico(id);
 	 
 	 
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_registro_colibri_grasa') != 0	
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_registro_colibri_grasa') 	
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_registro_colibri_grasa
 
 ALTER TABLE captura_colibri ADD CONSTRAINT FK_registro_colibri_grasa FOREIGN KEY (grasa) REFERENCES leyenda_grasa(id);
 	
 
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_registro_colibri_musculo') != 0
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_registro_colibri_musculo') 
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_registro_colibri_musculo
 			
-	ALTER TABLE captura_colibri ADD CONSTRAINT FK_registro_colibri_musculo  FOREIGN KEY (musculo) REFERENCES leyenda_musculo(id);
+ALTER TABLE captura_colibri ADD CONSTRAINT FK_registro_colibri_musculo  FOREIGN KEY (musculo) REFERENCES leyenda_musculo(id);
 
 
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_registro_colibri_protuberancia_cloacal') != 0		
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_registro_colibri_protuberancia_cloacal') 		
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_registro_colibri_protuberancia_cloacal
 
 ALTER TABLE captura_colibri ADD CONSTRAINT FK_registro_colibri_protuberancia_cloacal FOREIGN KEY (protuberancia_cloacal) REFERENCES leyenda_protuberancia_cloacal(id);
 
 
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_registro_colibri_parche_reproductivo') != 0		
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_registro_colibri_parche_reproductivo') 		
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_registro_colibri_parche_reproductivo
 
 ALTER TABLE captura_colibri ADD CONSTRAINT FK_registro_colibri_parche_reproductivo FOREIGN KEY (parche_reproductivo) REFERENCES leyenda_parche_reproductivo(id);
 
 
-IF(select COUNT (*) from sys.foreign_keys where name = 'FK_captura_colibri_anillador') != 0		
+IF EXISTS (select name from sys.foreign_keys where name = 'FK_captura_colibri_anillador') 	
 	ALTER TABLE captura_colibri  DROP CONSTRAINT FK_captura_colibri_anillador
 
 ALTER TABLE captura_colibri ADD CONSTRAINT FK_captura_colibri_anillador FOREIGN KEY (anillador) REFERENCES usuario(acronimo);
