1a0d545b13253f342ecc5f18be67be176cd0a9a5
Ender Puentes <endpuent@gmail.com>
2025-05-30 21:57:26 -0300

    feat: Update ExpenseCommentView to include user modification message and order by updated_at

--- definitions/views.sql
+++ definitions/views.sql
@@ -14,8 +14,9 @@ CREATE VIEW ExpenseCommentView AS
 SELECT 
     ec.id,
     ec.expense_id,
-    CONCAT(u.name, ec.comment) AS comment,
+    CONCAT(u.name, ' modificó esta transacción: ', ec.comment) AS comment,
     ec.created_at
 FROM ExpenseComment ec
 JOIN User u 
 ON ec.user_id = u.id
+ORDER BY ec.updated_at DESC;
